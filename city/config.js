e6f02b21609f61e3e8ecfc0bcdd87b6aa600b588
Cory Guynn <cory@premiereportal.net>
2015-10-22 00:57:31 +0100

    no message

---
+++ city/config.js
@@ -0,0 +1,10 @@
+// config.js
+// place API keys, etc here
+var config = {};
+
+config.pubnub = {};
+
+config.pubnub.publish_key = 'pub-c-da7f2ff7-6f33-4fdf-8713-7cf00975ccdd';
+config.pubnub.subscribe_key = 'sub-c-e7920ce4-6b9c-11e5-bcab-02ee2ddab7fe';
+
+module.exports = config;
